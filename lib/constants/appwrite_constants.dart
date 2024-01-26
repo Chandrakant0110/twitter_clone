8a6d937d56d8f6734dcbbd50df493b3189c57a94
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-01-26 08:12:18 +0530

    added constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const databaseId = '65b277f386b1120849f1';
+  static const projectId = '65b270da156bfa05300c';
+  static const endPoint = 'http://localhost:80/';
+}
