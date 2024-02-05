752ffa1adbfe08b37e5c36c51fba40476a1a39e7
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-05 12:38:16 +0530

    added appbar and body

--- lib/features/user_profile/views/edit_profile_view.dart
+++ lib/features/user_profile/views/edit_profile_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
@@ -17,36 +19,81 @@ class EditProfileView extends ConsumerStatefulWidget {
 }
 
 class _EditProfileViewState extends ConsumerState<EditProfileView> {
+  final nameController = TextEditingController();
+  final bioController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    bioController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(currentUserDetailsProvider).value;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+        // centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Save',
+              style: TextStyle(
+                color: Pallete.blueColor,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: user == null
           ? const Loader()
           : Column(
               children: [
                 SizedBox(
                   height: 200,
-                  child: Stack(children: [
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: user.bannerPic.isEmpty
-                          ? Container(
-                              color: Pallete.blueColor,
-                            )
-                          : Image.network(user.bannerPic),
-                    ),
-                    Positioned(
-                      bottom: 20,
-                      left: 20,
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(user.profilePic),
-                        radius: 40,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: user.bannerPic.isEmpty
+                            ? Container(
+                                color: Pallete.blueColor,
+                              )
+                            : Image.network(user.bannerPic),
                       ),
-                    ),
-                  ]),
+                      Positioned(
+                        bottom: 20,
+                        left: 20,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(user.profilePic),
+                          radius: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Name',
+                    contentPadding: EdgeInsets.all(18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: bioController,
+                  decoration: const InputDecoration(
+                    hintText: 'Bio',
+                    contentPadding: EdgeInsets.all(18),
+                  ),
+                  maxLines: 4,
                 ),
               ],
             ),
