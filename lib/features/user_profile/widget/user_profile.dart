2c8c69313c84d93f0a95f2bf7965520875af95ca
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-05 11:11:51 +0530

    donw with basicprofile view bar

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/models/user_model.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -13,28 +15,96 @@ class UserProfile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-            expandedHeight: 200,
-            floating: true,
-            snap: true,
-            flexibleSpace: Stack(
-              children: [
-                Positioned.fill(
-                  child: user.bannerPic.isEmpty
-                      ? Container(
-                          color: Pallete.blueColor,
-                        )
-                      : Image.network(user.bannerPic),
-                ),
-              ],
+    final currUser = ref.watch(currentUserDetailsProvider).value;
+
+    return Scaffold(
+      body: currUser == null
+          ? const Loader()
+          : NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    expandedHeight: 200,
+                    floating: true,
+                    snap: true,
+                    flexibleSpace: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: user.bannerPic.isEmpty
+                              ? Container(
+                                  color: Pallete.blueColor,
+                                )
+                              : Image.network(user.bannerPic),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(user.profilePic),
+                            radius: 45,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          margin: const EdgeInsets.all(20),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              print('edit profile/ follow button clicked');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                side: const BorderSide(
+                                  color: Pallete.whiteColor,
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 25,
+                              ),
+                            ),
+                            child: Text(
+                              currUser.uid == user.uid
+                                  ? 'Edit Profile'
+                                  : 'follow',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(8),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Text(
+                            user.name,
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '@${user.name}',
+                            style: const TextStyle(
+                              fontSize: 17,
+                              color: Pallete.greyColor,
+                            ),
+                          ),
+                          Text(
+                            user.bio,
+                            style: const TextStyle(
+                              fontSize: 17,
+                              color: Pallete.greyColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ];
+              },
+              body: Container(),
             ),
-          ),
-        ];
-      },
-      body: Text('hello this is the body of this scroll view'),
     );
   }
 }
