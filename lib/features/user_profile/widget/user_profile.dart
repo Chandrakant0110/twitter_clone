5ac1372431d4304fd791b36dd537cf4fb76144a8
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-05 12:05:49 +0530

    fixed bug

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -137,63 +137,81 @@ class UserProfile extends ConsumerWidget {
                     data: (tweets) {
                       return ref.watch(getLatestTweetProvider).when(
                             data: (data) {
-                              if (data.events.contains(
-                                  'databases.*.collections.${AppWriteConstants.tweetCollections}.documents.*.create')) {
-                                // condition is above
-                                tweets.insert(0, Tweet.fromMap(data.payload));
-                              } else if (data.events.contains(
-                                  'databases.*.collections.${AppWriteConstants.tweetCollections}.documents.*.update')) {
-                                // Steps for fetching the updates of retweeted in a App.
-                                // 1. get id of original tweet
-                                // 2. remove that tweet
-                                // 3. replace with the updated tweet
+                              final latestTweet = Tweet.fromMap(data.payload);
 
-                                // Step 1
-                                // find index of tweet from index array
-                                final startingPoint =
-                                    data.events[0].lastIndexOf('documents.');
+                              bool isTweetAlreadyPresent = false;
 
-                                final endPoint =
-                                    data.events[0].lastIndexOf('.update');
-                                // Finding the tweetID form the data.events
-                                final tweetID = data.events[0].substring(
-                                    startingPoint + 10,
-                                    endPoint); //10 = no. of characters in "documents."
-                                // fetching that particular tweet
-                                var tweet = tweets
-                                    .where((element) => element.id == tweetID)
-                                    .first;
-                                // fetching the tweetIndex before removing the tweet
-                                final tweetIndex = tweets.indexOf(tweet);
-                                // Step 2
-                                // remove the tweet
-                                tweets.removeWhere(
-                                    (element) => element.id == tweetID);
-                                // get new latest data
-                                tweet = Tweet.fromMap(data.payload);
-
-                                // Step 3
-                                // add the latest data
-                                tweets.insert(tweetIndex, tweet);
+                              for (final tweetModel in tweets) {
+                                if (tweetModel.id == latestTweet.id) {
+                                  isTweetAlreadyPresent = true;
+                                  break;
+                                }
                               }
-                              return ListView.builder(
-                                itemCount: tweets.length,
-                                itemBuilder: (context, index) {
-                                  final tweet = tweets[index];
-                                  return TweetCard(tweet: tweet);
-                                },
+
+                              if (!isTweetAlreadyPresent) {
+                                if (data.events.contains(
+                                    'databases.*.collections.${AppWriteConstants.tweetCollections}.documents.*.create')) {
+                                  // condition is above
+                                  tweets.insert(0, Tweet.fromMap(data.payload));
+                                } else if (data.events.contains(
+                                    'databases.*.collections.${AppWriteConstants.tweetCollections}.documents.*.update')) {
+                                  // Steps for fetching the updates of retweeted in a App.
+                                  // 1. get id of original tweet
+                                  // 2. remove that tweet
+                                  // 3. replace with the updated tweet
+
+                                  // Step 1
+                                  // find index of tweet from index array
+                                  final startingPoint =
+                                      data.events[0].lastIndexOf('documents.');
+
+                                  final endPoint =
+                                      data.events[0].lastIndexOf('.update');
+                                  // Finding the tweetID form the data.events
+                                  final tweetID = data.events[0].substring(
+                                      startingPoint + 10,
+                                      endPoint); //10 = no. of characters in "documents."
+                                  // fetching that particular tweet
+                                  var tweet = tweets
+                                      .where((element) => element.id == tweetID)
+                                      .first;
+                                  // fetching the tweetIndex before removing the tweet
+                                  final tweetIndex = tweets.indexOf(tweet);
+                                  // Step 2
+                                  // remove the tweet
+                                  tweets.removeWhere(
+                                      (element) => element.id == tweetID);
+                                  // get new latest data
+                                  tweet = Tweet.fromMap(data.payload);
+
+                                  // Step 3
+                                  // add the latest data
+                                  tweets.insert(tweetIndex, tweet);
+                                }
+                              }
+
+                              return Expanded(
+                                child: ListView.builder(
+                                  itemCount: tweets.length,
+                                  itemBuilder: (context, index) {
+                                    final tweet = tweets[index];
+                                    return TweetCard(tweet: tweet);
+                                  },
+                                ),
                               );
                             },
                             error: (error, stackTrace) => ErrorText(
                               error: error.toString(),
                             ),
                             loading: () {
-                              return ListView.builder(
-                                itemCount: tweets.length,
-                                itemBuilder: (context, index) {
-                                  final tweet = tweets[index];
-                                  return TweetCard(tweet: tweet);
-                                },
+                              return Expanded(
+                                child: ListView.builder(
+                                  itemCount: tweets.length,
+                                  itemBuilder: (context, index) {
+                                    final tweet = tweets[index];
+                                    return TweetCard(tweet: tweet);
+                                  },
+                                ),
                               );
                             },
                           );
