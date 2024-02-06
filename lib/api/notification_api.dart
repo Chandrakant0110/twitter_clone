0cfaa44894855ad5040ce74229f104646e4d1995
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-06 17:32:30 +0530

    fetch notification api created

--- lib/api/notification_api.dart
+++ lib/api/notification_api.dart
@@ -1,4 +1,5 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/constants/appwrite_constants.dart';
@@ -9,17 +10,22 @@ import 'package:twitter_clone/models/notification_model.dart';
 final notificationAPIProvider = Provider((ref) {
   return NotificationAPI(
     databases: ref.watch(appwriteDatabaseProvider),
+    realtime: ref.watch(appwriteRealtimeProvider),
   );
 });
 
 abstract class INotificationAPI {
   FutureEitherVoid createNotification(Notification notification);
+  Future<List<Document>> getNotifications(String uid);
+  Stream<RealtimeMessage> getLatestNotification();
 }
 
 class NotificationAPI extends INotificationAPI {
   final Databases _db;
-
-  NotificationAPI({required Databases databases}) : _db = databases;
+  final Realtime _realtime;
+  NotificationAPI({required Databases databases, required Realtime realtime})
+      : _realtime = realtime,
+        _db = databases;
 
   @override
   FutureEitherVoid createNotification(Notification notification) async {
@@ -42,4 +48,25 @@ class NotificationAPI extends INotificationAPI {
       return left(Failure(e.toString(), st));
     }
   }
+
+  @override
+  Future<List<Document>> getNotifications(String uid) async {
+    final documents = await _db.listDocuments(
+      databaseId: AppWriteConstants.databaseId,
+      collectionId: AppWriteConstants.notificationsCollection,
+      queries: [
+        Query.equal('uid', uid),
+      ],
+    );
+    return documents.documents;
+  }
+
+  @override
+  Stream<RealtimeMessage> getLatestNotification() {
+    return _realtime.subscribe([
+      'databases.${AppWriteConstants.databaseId}.collections.${AppWriteConstants.notificationsCollection}.documents'
+    ]).stream;
+  }
+
+  
 }
