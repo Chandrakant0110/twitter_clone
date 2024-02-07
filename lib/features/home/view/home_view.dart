5e246a98cc6db57681fa800942e76ea2932205f2
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-07 23:29:48 +0530

    drawer added in home screen

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/home/widgets/side_drawer.dart';
 import 'package:twitter_clone/features/tweet/views/create_tweet.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
@@ -36,9 +37,7 @@ class _HomeViewState extends State<HomeView> {
     return SafeArea(
       child: Scaffold(
         appBar: _page == 0 ? appBar : null,
-        // drawer: const Drawer(
-        //   child: Text('Icon'),
-        // ),
+        drawer: const SideDrawer(),
         body: IndexedStack(
           index: _page,
           children: UIConstants.bottomTabBarPages,
---
+++ lib/features/home/widgets/side_drawer.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/features/user_profile/views/user_profile_view.dart';
+import 'package:twitter_clone/theme/theme.dart';
+
+class SideDrawer extends ConsumerWidget {
+  const SideDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currUser = ref.watch(currentUserDetailsProvider).value;
+    return SafeArea(
+      child: Drawer(
+        backgroundColor: Pallete.backgroundColor,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.person,
+                size: 30,
+              ),
+              title: const Text(
+                'My Profile',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  UserProfileView.route(currUser!),
+                );
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                AssetsConstants.verifiedIcon,
+                height: 28,
+              ),
+              title: const Text(
+                'Twitter Blue',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.logout_outlined,
+                size: 30,
+              ),
+              title: const Text(
+                'Log Out',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
