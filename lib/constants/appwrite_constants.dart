ff6056a16fd1e5be7078908ff58a3c016b8b7325
Chandrakant Sahu <100430377+Chandrakant0110@users.noreply.github.com>
2024-02-07 12:29:36 +0530

    Updated

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -14,13 +14,5 @@ class AppWriteConstants {
   }
 
   // static const endPoint = 'http://localhost:80/v1'; // works forAPp
-  // static const endPoint = 'http://192.168.22.189:80/v1'; // --->works in webApp
 }
 
-class AppWriteConstantsOnline {
-  static const databaseId = '65b6a5f0cfa707300eb8'; 
-  static const projectId = '65b68bdfebca332becd9';
-  static const endPoint = 'https://cloud.appwrite.io/v1';
-  // static const endPoint = 'http://192.168.22.189:80/v1';
-  // static const endPoint = 'ws://127.0.0.1:59814/F4OXFJNgaG4=/ws';
-}
