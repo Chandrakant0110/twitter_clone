dc9a7db75af328bb8d8a12b987bb9d206b42c29e
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-02-11 10:18:20 +0530

    updated the backend

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,18 +1,34 @@
+// class AppWriteConstants {
+//   static const databaseId = '65b277f386b1120849f1';
+//   static const projectId = '65b270da156bfa05300c';
+//   static const endPoint = 'http://10.0.2.2:80/v1'; //emulator ip
+
+//   static const imagesBucket = '65bb905be63a8aed58e7';
+
+//   static const userCollections = '65b7b96f0c694c752402';
+//   static const tweetCollections = '65bb2c4dd75bf23bda50';
+//   static const notificationsCollection = '65c1abed68611744d4b8';
+
+//   static String imageUrl(String imageId) {
+//     return '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
+//   }
+
+//   // static const endPoint = 'http://localhost:80/v1'; // works forAPp
+// }
 class AppWriteConstants {
-  static const databaseId = '65b277f386b1120849f1';
-  static const projectId = '65b270da156bfa05300c';
-  static const endPoint = 'http://10.0.2.2:80/v1'; //emulator ip
+  
+  static const databaseId = '65c61a5ef0172db81a3e';
+  static const projectId = '65c6199c37e56265a2f2';
+  static const endPoint = 'https://cloud.appwrite.io/v1'; //emulator ip
 
-  static const imagesBucket = '65bb905be63a8aed58e7';
+  static const imagesBucket = '65c61ff213d0b181cb20';
 
-  static const userCollections = '65b7b96f0c694c752402';
-  static const tweetCollections = '65bb2c4dd75bf23bda50';
-  static const notificationsCollection = '65c1abed68611744d4b8';
+  static const userCollections = '65c61e823366616535a2';
+  static const tweetCollections = '65c61b8af25f20bceb95';
+  static const notificationsCollection = '65c61a74836de3e8a096';
 
   static String imageUrl(String imageId) {
     return '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
   }
 
-  // static const endPoint = 'http://localhost:80/v1'; // works forAPp
 }
-
