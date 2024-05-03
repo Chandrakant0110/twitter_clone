79cd9c900f8b4cf16e051362d7df179359d7a00a
Chandrakant Sahu <100430377+Chandrakant0110@users.noreply.github.com>
2024-05-03 11:56:05 +0530

    Create sample_secrets.dart

---
+++ lib/sample_secrets.dart
@@ -0,0 +1,21 @@
+//rename this file to secrets.dart
+
+class Secrets {
+  static const databaseId = 'YOUR_DATABASE_ID';
+  static const projectId = 'YOUR_PROJECT_ID';
+  static const endPoint = 'https://cloud.appwrite.io/v1'; //server ip
+
+  static const imagesBucket = 'YOUR_IMAGE_BUCKET_ID';
+
+  static const userCollections = 'YOUR_USER_COLLECTIONS_ID';
+  static const tweetCollections = 'YOUR_TWEET_COLLECTIONS_ID';
+  static const notificationsCollection = 'YOUR_NOTIFICAIONS_COLLECTIONS_ID';
+
+  static const googleClientId =
+      'YOUR_GOOGLE_CLIENT_ID';
+  static const googleClientSecret = 'YOUR_GOOGLE_CLIENT_SECRET';
+  
+  static const googleClientIdApp =
+      'YOUR_GOOGLE_CLIENT_APP_ID';
+  static const googleClientSecretApp = 'YOUR_GOOGLE_CLIENT_APP_SECRET';
+}
